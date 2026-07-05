6bfd924280e51f85515101534d942f3ef10de8e8
Brawijayadewa <164440446+Brawijayadewa@users.noreply.github.com>
2026-07-05 21:07:17 +0700

    ini

--- PROJECT EXPO/src/App.java
+++ PROJECT EXPO/src/App.java
@@ -1,7 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("UDH AKU UBAH");
     }
 }
-
-kkkkk
